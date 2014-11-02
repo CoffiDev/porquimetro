97bcd184f9050d7d25dad29c47fb5dd999ad9eb3
Alejandro Cuenca <alexcuenca4@gmail.com>
2014-11-01 23:48:48 -0600

    Bugs everywhere

--- app/src/main/java/com/example/alejandro/porquimetro/ArticuloMostrar.java
+++ app/src/main/java/com/example/alejandro/porquimetro/ArticuloMostrar.java
@@ -12,9 +12,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,26 +24,35 @@ import org.json.JSONObject;
 
 public class ArticuloMostrar extends Activity {
 
-    String url = "http://porkimetro.wc.lt/info.php?art=1&ley=1";
+    String url = "http://porkimetro.wc.lt/cons.php?ley=1&art=1";
     private TextView num;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articulo_mostrar);
         num = (TextView) findViewById(R.id.texto);
+        num.setText("Hola ");
+
+        Toast.makeText(this,"Hola",Toast.LENGTH_SHORT).show();
+
+          RequestQueue queue = Volley.newRequestQueue(this);
 
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
+                            Toast.makeText(getApplicationContext(),"Hola2",Toast.LENGTH_SHORT).show();
+                        num.setText("baakaa");
                         try {
                             num.setText("Numero: " + response.getString("numero")+"sgshsd");
                             Log.e("response",""+response.toString());
                             Toast.makeText(ArticuloMostrar.this,"It works",Toast.LENGTH_LONG).show();
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            num.setText("yolo");
                         }
 
                     }
@@ -49,12 +60,13 @@ public class ArticuloMostrar extends Activity {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        num.setText("juuum");
                         // TODO Auto-generated method stub
-
+                                    Toast.makeText(getApplicationContext(),"Hola3",Toast.LENGTH_SHORT).show();
                     }
                 });
+         queue.add(jsObjRequest);
 
-        super.onCreate(savedInstanceState);
     }
 
     public void inicio(View v) {
